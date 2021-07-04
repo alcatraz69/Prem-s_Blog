fc5265c746fe8443362658b5d1f70d83c3c112d3
Prem Kantikar <48849830+alcatraz69@users.noreply.github.com>
2021-07-04 10:03:18 +0530

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,7 @@ import { rhythm } from "../utils/typography"
 const Bio = () => {
   const data = useStaticQuery(graphql`
     query BioQuery {
-      avatar: file(absolutePath: { regex: "/pic2.jpeg/" }) {
+      avatar: file(absolutePath: { regex: "/newDp.png/" }) {
         childImageSharp {
           fixed(width: 80, height: 80) {
             ...GatsbyImageSharpFixed
